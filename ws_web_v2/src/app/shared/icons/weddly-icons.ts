39cb8600e5046fc351d3fefb21ae2d0fbcb8dbea
javierp311204 <jperez.salas31@gmail.com>
2026-05-06 12:23:25 +0200

    Fix home

--- ws_web_v2/src/app/shared/icons/weddly-icons.ts
+++ ws_web_v2/src/app/shared/icons/weddly-icons.ts
@@ -451,4 +451,9 @@ export const WeddlyIcons: Record<string, string> = {
   archive: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
   <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5zm13-3H1v2h14zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
 </svg>`,
+
+  cash: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash" viewBox="0 0 16 16">
+    <path d="M8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+    <path d="M0 4a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V6a2 2 0 0 1-2-2z"/>
+  </svg>`
 };
